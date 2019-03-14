8f405ef3ff8d21b54f06803239949a5215920b88
Riley Weber <rileyw13@protonmail.com>
2019-03-13 21:30:34 -0400

    solved

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,12 @@
 def is_isogram(string):
-    pass
+    s = set()
+    is_isogram = True
+    string = string.lower()
+    string = string.replace('-', '')
+    string = string.replace(' ', '')
+    for letter in string:
+        if letter not in s:
+            s.add(letter)
+        else:
+            is_isogram = False
+    return is_isogram
