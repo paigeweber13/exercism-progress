b0ae8c0921be7d196b260b4f1c1d6c42a38a0c39
Riley Weber <rileyw13@protonmail.com>
2019-03-14 14:48:35 -0400

    solved

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,2 +1,12 @@
+def getLyricsFromFile(fileName = 'lyrics.txt'):
+    with open(fileName) as lyricsFile:
+        verses = [line.rstrip('\n') for line in lyricsFile]
+    return verses
+
 def recite(start_verse, end_verse):
-    pass
+    start_verse -= 1
+    verses = getLyricsFromFile()
+    song = []
+    for i in range (start_verse, end_verse):
+        song.append(verses[i])
+    return song
