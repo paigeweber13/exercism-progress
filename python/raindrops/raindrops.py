05b0711de81b2521d97f8650cb74bd431bc286b7
Riley Weber <rileyw13@protonmail.com>
2019-03-13 21:19:13 -0400

    solved raindrop

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,15 @@
 def raindrops(number):
-    pass
+    result = ""
+    hasFactor = False
+    if number % 3 == 0:
+        result += "Pling"
+        hasFactor = True
+    if number % 5 == 0:
+        result += "Plang"
+        hasFactor = True
+    if number % 7 == 0:
+        result += "Plong"
+        hasFactor = True
+    if not hasFactor:
+        result += str(number)
+    return result
