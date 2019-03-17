5d3baeb0caca5d8786c1a57d1c52a1bbab2be97b
Riley Weber <rileyw13@protonmail.com>
2019-03-16 22:31:43 -0400

    soved

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,15 @@
+import json
+
+def load_scores(file_name = 'scores.json'):
+    scores = {}
+    with open(file_name) as f:
+        scores = json.load(f)
+    return scores
+
 def score(word):
-    pass
+    word = word.lower()
+    scores = load_scores()
+    score = 0
+    for letter in word:
+        score += scores[letter]
+    return score
