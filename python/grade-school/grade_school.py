12ea4db99ff9700dcc6d1c931afc978589d2d2b6
Riley Weber <rileyw13@protonmail.com>
2019-04-16 11:37:14 -0400

    all functions inmplemented

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,22 @@
+from student import Student
+
 class School(object):
     def __init__(self):
-        pass
+        self.students = []
 
     def add_student(self, name, grade):
-        pass
+        self.students.append(Student(name, grade))
+        self.students.sort()
 
     def roster(self):
-        pass
+        result = []
+        for student in self.students:
+            result.append(student.name)
+        return result
 
     def grade(self, grade_number):
-        pass
+        result = []
+        for student in self.students:
+            if student.grade == grade_number:
+                result.append(student.name)
+        return result
