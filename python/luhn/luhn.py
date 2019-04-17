ae395f2748091fa532d5408d4013c7bcbe68f99c
Riley Weber <rileyw13@protonmail.com>
2019-04-17 12:34:00 -0400

    made adjustments based on output of pylint

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,33 +1,29 @@
-class Luhn(object):
+class Luhn():
     def __init__(self, card_num_string):
         self.card_num = []
         card_num_string = card_num_string.replace(' ', '')
         for char in card_num_string:
             try:
                 self.card_num.append(int(char))
-            except ValueError as e:
+            except ValueError as exception:
                 print('only allowed input it numbers and spaces!')
-                print(e)
+                print(exception)
                 self.card_num = []
                 return
-
 
     def is_valid(self):
         card_num_len = len(self.card_num)
         if card_num_len < 2:
             return False
-        
+
         tmp_card_num = self.card_num.copy()
         for i in range(card_num_len - 2, -1, -2):
             tmp_card_num[i] *= 2
             if tmp_card_num[i] > 9:
                 tmp_card_num[i] -= 9
-        
-        sum = 0
+
+        card_num_sum = 0
         for num in tmp_card_num:
-            sum += num
-        
-        if sum % 10 == 0:
-            return True
-        else:
-            return False
+            card_num_sum += num
+
+        return bool(card_num_sum % 10 == 0)
