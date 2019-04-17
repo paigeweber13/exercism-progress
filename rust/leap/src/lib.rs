99ab990cdff862d857c4b9c9d79d23618a27c18b
Riley Weber <rileyw13@protonmail.com>
2019-04-17 11:41:10 -0400

    fixed formatting

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn is_leap_year(year: u64) -> bool {
-  if year % 4 == 0 && year % 100 != 0{
-    true
-  } else if year % 400 == 0{
-    true
-  } else {
-    false
-  }
+    if year % 4 == 0 && year % 100 != 0 {
+        true
+    } else if year % 400 == 0 {
+        true
+    } else {
+        false
+    }
 }
