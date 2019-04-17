7a09b168b5eadfc0286703974440bd76a0aea05e
Riley Weber <rileyw13@protonmail.com>
2019-04-17 11:38:19 -0400

    solved

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+  if year % 4 == 0 && year % 100 != 0{
+    true
+  } else if year % 400 == 0{
+    true
+  } else {
+    false
+  }
 }
