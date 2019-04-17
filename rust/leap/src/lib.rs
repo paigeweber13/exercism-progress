0134a601aa1b4a607dab054d7bb1faef4bae56e8
Riley Weber <rileyw13@protonmail.com>
2019-04-17 11:44:02 -0400

    followed some clippy advice to clean code

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,9 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 4 == 0 && year % 100 != 0 {
-        true
-    } else if year % 400 == 0 {
-        true
-    } else {
-        false
-    }
+    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
 }
