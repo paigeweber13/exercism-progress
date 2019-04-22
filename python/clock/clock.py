f0d90147496b74464fde92a1557740a791491a42
Riley Weber <rileyw13@protonmail.com>
2019-04-21 21:49:50 -0400

    solved

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,15 +1,41 @@
 class Clock(object):
     def __init__(self, hour, minute):
-        pass
+        self.hour = hour
+        self.minute = minute
+        self.fix_time()
 
     def __repr__(self):
-        pass
+        # return str(self.hour) + ':' + "{:2d}".format(self.minute)
+        return str(self.hour).zfill(2) + ':' + str(self.minute).zfill(2)
 
     def __eq__(self, other):
-        pass
+        if self.hour == other.hour and self.minute == other.minute:
+            return True
+        else:
+            return False
 
     def __add__(self, minutes):
-        pass
+        self.minute += minutes
+        self.fix_time()
+        return self
 
     def __sub__(self, minutes):
-        pass
+        self.minute -= minutes
+        self.fix_time()
+        return self
+
+    """
+    checks if self is a valid time and fixes it in place if not
+    """
+    def fix_time(self): 
+        while(self.minute >= 60):
+            self.minute -= 60
+            self.hour += 1
+        while(self.hour >= 24):
+            self.hour -= 24
+
+        while(self.minute < 0):
+            self.minute += 60
+            self.hour -= 1
+        while(self.hour < 0):
+            self.hour += 24
