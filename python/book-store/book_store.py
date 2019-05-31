668f6a59ece12451998035efcc3098c7b1270991
Brian Weber <brian.weber1337@gmail.com>
2019-05-31 14:15:38 -0400

    made it work with multiple sets of books, but realized this is not the correct behavior

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,31 +1,38 @@
 def total(basket):
     PRICE_PER_BOOK = 800
 
-    different_books = set()
-    num_additional_books = 0
+    book_sets = [set()]
     for book in basket:
-        if book not in different_books:
-            different_books.add(book)
-        else:
-            num_additional_books += 1
+        book_added = False
+        for book_set in book_sets:
+            if book not in book_set:
+                book_set.add(book)
+                book_added = True
+                break
+        if not book_added:
+            book_sets.append(set([book]))
 
     total_cost = 0
 
-    # TODO: edge case where you buy a set of books twice. Each set gets a 
+    # DONE: edge case where you buy a set of books twice. Each set gets a 
     # discount
-
-    num_from_set = len(different_books)
-    total_cost += num_from_set * PRICE_PER_BOOK 
-    discount = 0
-    if num_from_set == 2:
-        discount = 0.05
-    elif num_from_set == 3:
-        discount = 0.1
-    elif num_from_set == 4:
-        discount = 0.2
-    elif num_from_set == 5:
-        discount = 0.25
-    total_cost *= (1 - discount)
     
-    total_cost += num_additional_books * PRICE_PER_BOOK
+    # IT's NOT THAT SIMPLE. YOU HAVE TO FIND THE MAXIMUM DISCOUNT YEESH
+    # add books to smallest set?
+
+    for book_set in book_sets:
+        num_from_set = len(book_set)
+        cost_from_this_set = num_from_set * PRICE_PER_BOOK 
+        discount = 0
+        if num_from_set == 2:
+            discount = 0.05
+        elif num_from_set == 3:
+            discount = 0.1
+        elif num_from_set == 4:
+            discount = 0.2
+        elif num_from_set == 5:
+            discount = 0.25
+        cost_from_this_set *= (1 - discount)
+        total_cost += cost_from_this_set
+    
     return total_cost
