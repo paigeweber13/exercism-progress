6df41879a4ad8034b60025c8a969a18bc5417cad
Riley <rileyw13@protonmail.com>
2019-09-17 12:52:45 -0400

    completed exercise

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,4 +1,7 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year
+  | not (mod year 100 == 0) && mod year 4 == 0 = True
+  | mod year 100 == 0 && mod year 400 == 0 = True
+  | otherwise = False
