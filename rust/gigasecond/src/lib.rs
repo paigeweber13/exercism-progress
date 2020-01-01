1f207312c7f37f8ac3899eb21190a3f865e56c0b
Riley <rileyw13@protonmail.com>
2020-01-01 16:27:11 -0500

    fixed solution

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, Duration};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + 1000000000
+    start + chrono::Duration::seconds(1000000000)
 }
